8e7516dd6eb1603c0a83dfa94d1af4dec6e1cbc1
ikhsan <ikhsanmuhamad3885@gmail.com>
2025-01-09 13:49:13 +0700

    add: button role dice

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,30 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:first_app/styled_text.dart';
 
 const startAligment = Alignment.topLeft;
 const endAligment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.color1, this.color2, {super.key});
+  GradientContainer(this.color1, this.color2, {super.key});
 
   final Color color1;
   final Color color2;
+  var actveDiceImage = 'assets/images/dice-1.png';
+
+  void rolldice() {
+    actveDiceImage = 'assets/images/dice-2.png';
+    print('Changing');
+  }
 
   @override
   Widget build(context) {
     return Container(
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-        colors: [color1, color2],
-        begin: startAligment,
-        end: endAligment,
-      )),
-      child: Center(
-          child: Image.asset(
-        'assets/images/dice-1.png',
-        width: 200,
-      )),
-    );
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAligment,
+          end: endAligment,
+        )),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                actveDiceImage,
+                width: 200,
+              ),
+              SizedBox(height: 20),
+              TextButton(
+                  onPressed: rolldice,
+                  style: TextButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      textStyle: TextStyle(fontSize: 28)),
+                  child: Text('Roll dice'))
+            ],
+          ),
+        ));
   }
 }
 
